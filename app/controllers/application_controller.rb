ed3acb137fc9192b1b174abf13e7fc89e30e4219
pedrito <pedrito.gemelo@gmail.com>
2016-06-15 10:35:17 +0000

    23 Se utilizo la herencia de partials con el application_controller y main_controller y se creo un archivo en layouts que es landing.haml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  #Para que implemente del metodo set_layout
+  layout :set_layout
+
+  #Hace el render del aplication
+  protected
+	  def set_layout
+	  	"application"
+	  end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,4 +5,16 @@ class MainController < ApplicationController
   def unregistered
   end
   
+  #Proteje al metodo set
+  protected
+    #llam al metodo de la clase padre para mandar a llamar
+    #a landing y la vista es unregistered
+    #si es la retorna y si no pues sigue la ejecuion del codigo
+    #en este caso trae a la vista de application.html que
+    #se encuenta en applica_controller
+	  def set_layout
+	  	return "landing" if action_name == "unregistered"
+	  	super
+	  end
+	  
 end
