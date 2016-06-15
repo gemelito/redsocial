4dcce33b37e730a1bb4902542261b9f6db1d6842
pedrito <pedrito.gemelo@gmail.com>
2016-06-15 08:59:17 +0000

    18 se trabajo el error del feilure y se termino el login de facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,10 +12,41 @@ class Users::OmniauthCallbacksController < ApplicationController
 			sign_in_and_redirect @user, event: :authentication
 			return
 		end
-
+        #Guarda la informacion del usaurio en una variable de session
 		session["devise.auth"] = request.env["omniauth.auth"]
-
+        
+        #llama a la vista edit de users/omniauth_callbaks
+        #por si el usuario no cuanta con su email
 		render :edit
 	end
-
+	
+	#Madejando el error cuando el usuario da cancelar el permito con facebook
+	def failure
+	    #Lo direcciona al login con el error de facebook diciendo el motivo
+		redirect_to new_user_session_path, notice: "No pudimos loguearte. Error: #{params[:error_description]}. Motivo: #{params[:error_reason]}"
+	end
+	
+	#Metodo para operar los datos para completar el login
+	def custom_sign_up
+	    #Intancia al modelo User y llama al metodo from_omniauth y pasa los
+	    #parametros que requiere
+		@user = User.from_omniauth(session["devise.auth"])
+		#Pregunta si existen los parametros
+		if @user.update(user_params)
+		    
+			sign_in_and_redirect @user, event: :authentication
+		else
+			render :edit
+		end
+	end
+	
+	#Pametros fuertes	
+    private
+        #Metodo para actulizar o completar el login
+		def user_params
+			#Strong Params
+			#Require de una table user con un nombre, nombre de usuario y el email
+			#No hay mas parametros en el formulario rechaza la custom_sign_up
+			params.require(:user).permit(:name,:username,:email)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   	omniauth_callbacks: "users/omniauth_callbacks"
   }
   root 'main#home'
+  
+  #controlador para el formulario edit
+  post "/custom_sign_up", to: "users/omniauth_callbacks#custom_sign_up" 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
