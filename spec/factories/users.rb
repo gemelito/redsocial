eff1fbd271f0a6e8512e88d51a510acc405b570b
pedrito <pedrito.gemelo@gmail.com>
2016-06-15 12:22:11 +0000

    Se agrego un bloque de codigo en factoria para el model user y pruevas para el mismo en user_rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
-  factory :user do
-    
+ factory :user do
+    password "12345678"
+    #pasamos el campo que se llenara de manera dinamica
+    sequence(:email){|n| "dummy_#{n}@factory.com" }
+    sequence(:username){|n| "username#{n}" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe User, type: :model do
 	it { should validate_uniqueness_of :username }
 	
 	it { should validate_length_of(:username).is_at_least(3).is_at_most(12) }
+	
+	describe "#validate_username_regex" do
+	  let(:user){FactoryGirl.build(:user)}
+
+	  it "should not allow username with numbers at the beginning" do
+	  	user.username = "9asdasd"
+	  	expect(user.valid?).to be_falsy
+	  end
+
+	  it "should not contain special characters" do
+	  	user.username = "asdasd*"
+	  	expect(user.valid?).to be_falsy
+	  end
+
+	end
 end
